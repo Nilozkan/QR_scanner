071fce1b33cc9e68772043fba636357e1db13a83
Faruk Paçacı <farukpacaci1@gmail.com>
2023-12-21 19:45:13 +0300

    fix: qr code

--- lib/qr_scanner.dart
+++ lib/qr_scanner.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:qr_code_scanner/result_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 const bgColor = Color(0xfffafafa);
 
 class QRScanner extends StatefulWidget {
@@ -12,28 +12,103 @@ class QRScanner extends StatefulWidget {
 }
 
 class _QRScannerState extends State<QRScanner> {
-  bool isScanCompleted =false;
+  bool scannerStarted = false;
+  bool isScanCompleted = false;
+  late MobileScannerController _mobileScannerController;
+  String willShowText = "Scanning...";
+  late BarcodeCapture _barcodeCapture;
+
+  @override
+  void initState() {
+    super.initState();
+    _mobileScannerController = MobileScannerController();
+    // _mobileScannerController = MobileScannerController(
+    //   autoStart: true,
+    //   cameraResolution: const Size(720, 1280),
+    //   detectionSpeed: DetectionSpeed.normal,
+    //   torchEnabled: false,
+    //   detectionTimeoutMs: 1200,
+    //   facing: CameraFacing.back,
+    // );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _mobileScannerController.dispose();
+  }
 
   void closeScreen() {
     isScanCompleted = false;
   }
 
+  void handleOnDetect(BarcodeCapture capture) {
+    if (capture.barcodes.first.type == BarcodeType.url) {
+      launchUrl(Uri.parse(capture.barcodes.first.url!.url));
+      setState(() {
+        willShowText = capture.barcodes.first.url!.url;
+      });
+    }
+  }
+
+  Widget errorBuilder = Container(
+    width: double.maxFinite,
+    height: 200,
+    decoration: BoxDecoration(
+      color: Colors.grey[200],
+      borderRadius: BorderRadius.circular(8),
+    ),
+    child: const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.error,
+            color: Colors.black,
+          ),
+          Text(
+            "Error",
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: bgColor,
       appBar: AppBar(
         centerTitle: true,
         title: const Text(
-        "QR Scanner",
-         style: TextStyle(
+          "QR Scanner",
+          style: TextStyle(
             color: Colors.black87,
             fontSize: 18,
             fontWeight: FontWeight.bold,
             letterSpacing: 1,
-          )
+          ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                willShowText = "Scanning...";
+              });
+            },
+            icon: const Icon(
+              Icons.refresh,
+            ),
+          ),
+        ],
       ),
       body: Container(
         width: double.infinity,
@@ -45,34 +120,64 @@ class _QRScannerState extends State<QRScanner> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text("Plaace the QR code in the area",
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1,
-                   )
-                  ),
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1,
+                      )),
                   SizedBox(
                     height: 10,
                   ),
-                  Text("Scanning will be starting automatically",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                  ),),
+                  Text(
+                    "Scanning will be starting automatically",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
                 ],
-              )),
-          
-              Expanded(child: Container(child: const Text(
-                "Developed by Nil Ozkan",
-                 style: TextStyle(
+              ),
+            ),
+            SizedBox(
+              width: screenWidth * 0.8,
+              height: screenWidth * 0.8,
+              child: Builder(builder: (context) {
+                if (_mobileScannerController.isStarting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return MobileScanner(
+                  errorBuilder: (context, error, widget) => errorBuilder,
+                  fit: BoxFit.cover,
+                  onDetect: (capture) => handleOnDetect(capture),
+                  onScannerStarted: (scannerArguments) {
+                    setState(() {
+                      scannerStarted = true;
+                    });
+                  },
+                );
+              }),
+            ),
+            Text(
+              willShowText,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const Expanded(
+              child: SizedBox(
+                child: Text(
+                  "Developed by Nil Ozkan",
+                  style: TextStyle(
                     color: Colors.black87,
                     fontSize: 14,
                     letterSpacing: 1,
-                   )
-                ),)),
-              ]
+                  ),
+                ),
               ),
+            ),
+          ],
+        ),
       ),
     );
   }
